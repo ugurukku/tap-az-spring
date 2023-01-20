c748d05d1bf894b1946befbe82145affb0cf9cf4
Ugur <ugurkarimli1@gmail.com>
2023-01-20 16:19:32 +0400

    intial

---
+++ src/main/java/com/ugurukku/tapazspring/TapAzSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ugurukku.tapazspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapAzSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapAzSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ugurukku/tapazspring/TapAzSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugurukku.tapazspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapAzSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
