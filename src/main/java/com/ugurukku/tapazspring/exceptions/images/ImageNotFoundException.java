0e56ce69c6873cc787e2c083907ed7c3756866ef
Ugur <ugurkarimli1@gmail.com>
2023-02-05 02:48:06 +0400

    unimportant changes

--- src/main/java/com/ugurukku/tapazspring/exceptions/images/ImageNotFoundException.java
+++ src/main/java/com/ugurukku/tapazspring/exceptions/images/ImageNotFoundException.java
@@ -1,11 +1,11 @@
-package com.ugurukku.tapazspring.exceptions.product;
+package com.ugurukku.tapazspring.exceptions.images;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class CategoryNotFoundException extends RuntimeException{
-    public CategoryNotFoundException(String message) {
+public class ImageNotFoundException extends RuntimeException{
+    public ImageNotFoundException(String message) {
         super(message);
     }
 }
