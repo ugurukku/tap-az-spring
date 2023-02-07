49177d61394305698a269c1d6afea164270591c9
Ugur <ugurkarimli1@gmail.com>
2023-02-07 23:48:08 +0400

    ADDING password config

---
+++ src/main/java/com/ugurukku/tapazspring/security/PasswordConfig.java
@@ -0,0 +1,8 @@
+package com.ugurukku.tapazspring.security;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PasswordConfig {
+}
--- src/main/java/com/ugurukku/tapazspring/security/SecurityConfig.java
+++ src/main/java/com/ugurukku/tapazspring/security/SecurityConfig.java
@@ -5,21 +5,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Bean
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/products","/", "index", "/css/*", "/js/*")
+                .antMatchers(HttpMethod.GET, "/products", "/css/*", "/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
+                .formLogin()
+                .disable()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails ugur = User.builder()
+                .username("huseyn")
+                .password("huseyn2003")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ugur);
     }
 }
