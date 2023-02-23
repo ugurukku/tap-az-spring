bfc88364c05eb0b853ae15979dd9e04dbd7f7914
Ugur <ugurkarimli1@gmail.com>
2023-02-23 23:31:21 +0400

    working on get products by user id

--- src/main/java/com/ugurukku/tapazspring/entities/Product.java
+++ src/main/java/com/ugurukku/tapazspring/entities/Product.java
@@ -9,7 +9,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;//
+    private Long id;
 
     @Column(name = "title")
     private String title;
