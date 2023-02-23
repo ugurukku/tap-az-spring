3c61a748ad716ec47ed5205bd013eb15d05bacea
Ugur <ugurkarimli1@gmail.com>
2023-02-23 23:13:10 +0400

    working on get products by user id

--- src/main/java/com/ugurukku/tapazspring/controllers/ProductController.java
+++ src/main/java/com/ugurukku/tapazspring/controllers/ProductController.java
@@ -33,6 +33,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getById(@PathVariable(name = "id") Long id) {
+        System.out.println("Accepted");
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
