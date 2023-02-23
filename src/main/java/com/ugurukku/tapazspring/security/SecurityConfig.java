af163ea9ddbe87f0478999cec145491e667658d1
Ugur <ugurkarimli1@gmail.com>
2023-02-23 22:48:15 +0400

    working on register logic

--- src/main/java/com/ugurukku/tapazspring/security/SecurityConfig.java
+++ src/main/java/com/ugurukku/tapazspring/security/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.ugurukku.tapazspring.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
